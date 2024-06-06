257540b6a5316c3766273c5794fb1e470f8719ad
tormjo <tormodjordbakke@gmail.com>
2024-06-06 14:22:32 -0400

    Budget update fixed. Currency change still missing

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,10 +2,14 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: event.target.value,
+        });
     }
     return (
 <div className='alert alert-secondary'>
