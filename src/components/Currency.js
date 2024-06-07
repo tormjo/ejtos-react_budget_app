6309675bb3ef61fa189bc96ed555f557b1f1d86b
tormjo <tormodjordbakke@gmail.com>
2024-06-07 10:17:08 -0400

    Currency.js added

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -4,17 +4,24 @@ import { AppContext } from '../context/AppContext';
 const Currency = () => {
   const{dispatch, currency}= useContext(AppContext);
 
-  const {currency, setCurrency} = useState(currency);
+  const {setNewCurrency} = useState(currency);
+  const handleCurrencyChange = (event) => {
+    setNewCurrency(event.target.value)
+    dispatch({
+        type: 'CHG_CURRENCY',
+        payload: event.target.value,
+    });
+  } 
 
   return (
-<div className='alert alert-secondary'>
-    <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setAction(event.target.value)}>
-                  <option defaultValue value="£" name="GBP">£</option>
-                  <option value="$" name="USD">$</option>
-                  </select>
+    <div className='alert alert-secondary'>
+        <select className="custom-select" id="inputGroupSelect01" onChange={handleCurrencyChange}>
+                    <option defaultValue value="£" name="GBP">£ Pound</option>
+                    <option value="$" name="USD">$ Dollar</option>
+                    </select>
 
 
-</div>
+    </div>
     );
 
 };
