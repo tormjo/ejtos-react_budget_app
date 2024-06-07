9d8feb8a79bd9d92cfd20e77e1d7b68e28b57b89
tormjo <169053601+tormjo@users.noreply.github.com>
2024-06-07 14:46:41 +0200

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,13 +2,13 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { dispatch, budget, totExp } = useContext(AppContext);
+    const { dispatch, budget, totExp, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         if (event.target.value > 20000){
             alert("The value cannot exceed £ 20,000");
         } else if (event.target.value < totExp){
-            alert("The budget cannot be reduced to a value lower than the current spending £ " + totExp);
+            alert("The budget cannot be reduced to a value lower than the current spending " +  currency + totExp);
         } else {
             setNewBudget(event.target.value);
             dispatch({
